0fe768cb7b8ed8d3a4525be4c8b25f18867a5d07
chernykhev <chernykhev@it2g.ru>
2020-11-25 16:51:07 +0300

    end of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { Grid, GridColumn } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 import ActivityDetails from '../details/ActivityDetails'
@@ -14,7 +14,9 @@ interface IProps {
     setSelectedActivity: (activity: IActivity | null) => void;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
-    deleteActivity: (id: string) => void;
+    deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+    submitting: boolean;
+    target: string;
 }
 
 const ActivityDashboard: React.FC<IProps> = (
@@ -27,7 +29,9 @@ const ActivityDashboard: React.FC<IProps> = (
         editMode,
         setEditMode,
         setSelectedActivity,
-        deleteActivity
+        deleteActivity,
+        submitting,
+        target
     }) => {
     return (
         <Grid>
@@ -35,7 +39,9 @@ const ActivityDashboard: React.FC<IProps> = (
                 <ActivityList
                     activities={activities}
                     selectActivity={selectActivity}
-                    deleteActivity={ deleteActivity} />
+                    deleteActivity={ deleteActivity}
+                    submitting={submitting}
+                    target ={target} />
             </GridColumn>
             <GridColumn width={6}>
                 {selectedActivity && !editMode &&
@@ -51,7 +57,8 @@ const ActivityDashboard: React.FC<IProps> = (
                         setEditMode={setEditMode}
                         activity={selectedActivity!}
                         createActivity={createActivity}
-                        editActivity={editActivity} />}
+                        editActivity={editActivity} 
+                        submitting={submitting}/>}
             </GridColumn>
         </Grid>
     )
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,20 +1,22 @@
 import React, { FormEvent, useState } from 'react'
 import { Button, Form, Segment } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 interface IProps {
     setEditMode: (editMode: boolean) => void;
     activity: IActivity;
     createActivity: (activity: IActivity) => void;
     editActivity: (activity: IActivity) => void;
+    submitting: boolean
 }
 
 const ActivityForm: React.FC<IProps> = ({
     setEditMode,
     activity: initializeFormState,
     createActivity,
-    editActivity
+    editActivity,
+    submitting
 }) => {
 
     const initializeForm = () => {
@@ -89,6 +91,7 @@ const ActivityForm: React.FC<IProps> = ({
                     placeholder='Venue'
                     value={activity.venue} />
                 <Button
+                    loading={submitting}
                     floated='right'
                     positive type='submit'
                     content='Submit' />
